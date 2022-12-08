a1affaa9feca0c05b9820397ceb6269850047903
lucian lin <lucian_lin@cmoney.com.tw>
2022-12-08 10:45:41 +0800

    test2

---
+++ config.js
@@ -0,0 +1,3 @@
+require('dotenv').config()
+
+export const HELLO = process.env.HELLO
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
-import { HELLO } from '../config';
+import { HELLO } from './config';
 
 console.log(HELLO)
 export default function () {
