a00f7c4967a7822003faae26a6e66b7a35e5f01f
lucian lin <lucian_lin@cmoney.com.tw>
2022-12-08 11:03:45 +0800

    1234

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-const HELLO = process.env.HELLO
+const HELLO = __ENV.HELLO
 console.log(HELLO)
 export default function () {
   const res = http.get('https://test.k6.io');
